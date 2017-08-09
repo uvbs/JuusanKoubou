c350471213ca6c0b446240351a782bf97250a337
F・ノバルティス <Hiromi.Kaede@gmail.com>
2017-08-09 20:08:20 +0800

    sync

--- TempSync/make_static_link.bat
+++ TempSync/make_static_link.bat
@@ -6,5 +6,4 @@ mklink /j "SublimeText Installed Packages" "D:\Dev\SublimeText\Data\Installed Pa
 mklink /j "SublimeText Packages" "D:\Dev\SublimeText\Data\Packages"
 mklink /j "VSCodeUserData" "D:\Dev\VSCode\UserData"
 mklink /j "edao_savedata" "E:\Game\Falcom\ED_AO\savedata"
-mklink /j "D:\Desktop\Python3" "D:\Dev\PortablePython3"
 mklink /j "Vim" "D:\Dev\Vim"
